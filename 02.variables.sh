07e45c5b8cf40f9b675413cb7c32c1c25ff3cfa0
Prasnna <mrpkreddy94@gmail.com>
2025-07-09 21:45:25 +0530

    practing second script

---
+++ 02.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Number=$1
+
+if [ $Number -ge 50 ]
+then 
+    echo "the given number $Number is greater than 50"
+esle
+    echo "the given number $Number is less than or equal to 50"
+if    
