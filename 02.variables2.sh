3574f351ac397e5fb410dde68e0d092a7921ffdc
Prasnna <mrpkreddy94@gmail.com>
2025-07-10 11:08:37 +0530

    second script for practice

--- 02.variables2.sh
+++ 02.variables2.sh
@@ -1,9 +1,9 @@
 #1/bin/bash
 
-$PERSON1=ramesh
-$PERSON2=suresh
+PERSON1=ramesh
+PERSON2=suresh
 
-echo "$PERSON1:Hello $PERSON2 ,Good morning"
-echo "$PERSON2:Hi $PERSON1 , very good morning"
-echo "$PERSON1:How are you $PERSON2?"
-echo "$PERSON2:I am good $PERSON1,how are you?"
+echo "$PERSON1: Hello $PERSON2 ,Good morning"
+echo "$PERSON2: Hi $PERSON1 , very good morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1,how are you?"
