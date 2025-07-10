61b4d51550e6ec52b2080ea26f683e1cb8804a87
Prasnna <mrpkreddy94@gmail.com>
2025-07-10 12:48:03 +0530

    5th script

--- 07.arrays.sh
+++ 07.arrays.sh
@@ -4,8 +4,8 @@ FRUITS=("apple" "banana" "Manago")
 
 echo "first value: ${FRUITS[0]}"
 
-echo "second value: ${[FRUITS[1]]}"
+echo "second value: ${FRUITS[1]}"
 
-echo "THRIRD value: ${[FRUITS[2]]}"
+echo "THRIRD value: ${FRUITS[2]}"
 
-echo "All value: ${[FRUITS@1]]}
+echo "All value: ${FRUITS[@]}
